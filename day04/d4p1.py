2cbaffd88da1cf55e111d8460c19e073c9babd60
J-JLucas <jlucas205@protonmail.com>
2022-12-22 15:19:58 -0700

    day4 part1 done

---
+++ day04/d4p1.py
@@ -0,0 +1,51 @@
+# This script reads two pairs of ranges from a file
+# and determines if one range is entirely encapulated by the other
+# prints count of total encapulations
+
+def read_int(a, i):
+    '''
+    returns a substring terminated by delimiters '-' or ',' or '\n' 
+    inputs: string a, int i
+    outputs: string substring, int i at value of current point in array
+    '''
+    substring = ""
+    while a[i] != "-" and a[i] != "," and a[i] != "\n":
+        substring = substring + a[i]
+        i+= 1
+    return substring,i
+
+def main():
+    f = open("d4.txt", "r")
+    total = 0
+    # for each line parse start and end point of both ranges
+    for line in f.readlines():
+        # intialize values
+        i = 0
+        x1 = ""
+        y1 = ""
+        x2 = ""
+        y2 = ""
+        # parse values
+        x1,i = read_int(line, i)
+        i+=1
+        y1,i = read_int(line, i)
+        i+=1
+        x2,i = read_int(line, i)
+        i+=1
+        y2,i = read_int(line, i)
+        # convert strings to ints
+        x1 = int(x1)
+        x2 = int(x2)
+        y1 = int(y1)
+        y2 = int(y2)
+
+        # determine if one range is fully encapuslated by the other
+        if x1 == x2:        # if x1 = x2, then one of the sets is for sure encapulated by the other,doesn't matter which
+            total+=1
+        elif x1 < x2 and y1 >= y2:
+                total+=1
+        elif x2 < x1 and y2 >= y1:
+                total+=1
+    print("Total number of encapusalted ranges =", total)
+    f.close()
+main()
